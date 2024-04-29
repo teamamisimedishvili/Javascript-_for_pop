c25db58a9fe101bc2ebf269311348d914e9fd478
teamamisimedishvili <127042618+teamamisimedishvili@users.noreply.github.com>
2024-04-30 00:27:16 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+var namber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < 10; i++) {
+
+    namber.pop();
+
+}
+
+document.write(namber);
+
+
+// var m = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for(let i=0; i < m.length; i++) {
+//     console.log(m[i])
+// }
